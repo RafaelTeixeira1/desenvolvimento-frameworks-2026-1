d84e955741f607897a2fd45740ba23dbf641f7b0
rafaelteixeira1 <rrafael_st@hotmail.com>
2026-03-06 21:22:30 -0300

    Adiciona chave entrangeira categoria_id nas tarefas

--- lista-tarefas-laravel/src/app/Models/Tarefa.php
+++ lista-tarefas-laravel/src/app/Models/Tarefa.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Categoria;
 
 class Tarefa extends Model
 {
-    protected $fillable = ['titulo', 'descricao', 'status'];
+    protected $fillable = ['titulo', 'descricao', 'status', 'categoria_id'];
+
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
 }
